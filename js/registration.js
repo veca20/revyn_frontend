7d0b963a6c309e023cf8fa4c9b18b3fab9c9f2b5
diak <diak@mesterlevel.lo>
2025-02-19 10:28:14 +0100

    login

--- js/registration.js
+++ js/registration.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const psw = document.getElementById('psw').value;
 
         try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+            const response = await fetch('https://nodejs314.dszcbaross.edu.hu/api/registration', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
