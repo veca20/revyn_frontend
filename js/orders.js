d3a4b4d0d4ad5e802a985478db4a24c8eaaf7e2a
kpisti18 <k.istvanzoltan@gmail.com>
2025-03-14 13:05:00 +0100

    getOrders

--- js/orders.js
+++ js/orders.js
@@ -1,24 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    function fetchOrders() {
-        fetch('/api/orders')
-            .then(response => response.json())
-            .then(orders => {
-                const mainContent = document.querySelector('.main-content');
-                mainContent.innerHTML = '<h1>Orders</h1><table><tr><th>Name</th><th>Address</th><th>Billing Info</th></tr></table>';
-                const table = mainContent.querySelector('table');
+document.addEventListener('DOMContentLoaded', getOrders); 
 
-                orders.forEach(order => {
-                    const row = document.createElement('tr');
-                    row.innerHTML = `
-                        <td>${order.name}</td>
-                        <td>${order.address}</td>
-                        <td>${order.billInformation}</td>
-                    `;
-                    table.appendChild(row);
-                });
-            })
-            .catch(error => console.error('Error fetching orders:', error));
-    }
+                // mainContent.innerHTML = '<h1>Orders</h1><table><tr><th>Name</th><th>Address</th><th>Billing Info</th></tr></table>';
+                // const table = mainContent.querySelector('table');
 
-    fetchOrders();
-});
+                // orders.forEach(order => {
+                //     const row = document.createElement('tr');
+                //     row.innerHTML = `
+                //         <td>${order.name}</td>
+                //         <td>${order.address}</td>
+                //         <td>${order.billInformation}</td>
+                //     `;
+                //     table.appendChild(row);
+                // });
+
+async function getOrders() {
+    const res = await fetch('/api/orders', {
+        method: 'GET',
+        credentials: 'include'
+    });
+
+    const orders = await res.json();
+    console.log(orders);
+    
+}
