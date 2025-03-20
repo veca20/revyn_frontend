c9dd2158779d63d365c91a19924bf2823048ebb6
diak <diak@mesterlevel.lo>
2025-03-20 09:07:20 +0100

    termékek meg jelenítése a product htmlben

--- js/product.js
+++ js/product.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.querySelector('.hamburger-menu');
     const navMenu = document.querySelector('nav ul');
 
-
     if (hamburger && navMenu) {
         hamburger.addEventListener('click', function () {
             navMenu.classList.toggle('show');
@@ -11,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         console.error('Hamburger vagy navMenu elem nem található');
     }
 });
+
 
 document.addEventListener('DOMContentLoaded', async function () {
     const params = new URLSearchParams(window.location.search);
