a7dada455064bdc938bef73d8392035d639c3948
diak <diak@mesterlevel.lo>
2025-03-25 11:05:34 +0100

    kijelentkezés gomb

--- js/editproduct.js
+++ js/editproduct.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     const productItem = document.createElement('div');
                     productItem.classList.add('product-item');
                     productItem.innerHTML = `
-                        <img src="${product.product_image_url}" alt="Product Image">
+                        <img src="${product.product_image}" alt="Product Image">
                         <div class="product-details">
                             <h2>${product.product_name}</h2>
                             <p>$${product.product_price}</p>
