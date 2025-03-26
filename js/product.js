446aaa278f7ff6e4614b035afb8ee6b99af65e30
kpisti18 <k.istvanzoltan@gmail.com>
2025-03-26 08:48:06 +0100

    product html fetch bugfix

--- js/product.js
+++ js/product.js
@@ -27,15 +27,16 @@ document.addEventListener('DOMContentLoaded', async function () {
 
         const product = await response.json();
         console.log(product);
-        
+        /*
         if (!product || !product.name || !product.price || !product.image || !product.description) {
             throw new Error('A termék adatai nem teljesek.');
         }
+        */
 
-        document.getElementById('product_name').textContent = product.name;
+        document.getElementById('product_name').textContent = `${product.name}`;
         document.getElementById('product_price').textContent = `Ár: $${product.price}`;
         document.getElementById('product_image').src = `uploads/${product.image}`;
-        document.getElementById('product_description').textContent = product.description;
+        document.getElementById('product_description').textContent = `${product.description}`;
 
         const addToCartButton = document.getElementById('add-to-cart');
         if (addToCartButton) {
