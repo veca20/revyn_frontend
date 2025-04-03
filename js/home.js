2c83558c19d5ad17f1d89fe70ac49b7a8a84ab74
diak <diak@mesterlevel.lo>
2025-04-03 12:27:08 +0200

    cupon

--- js/home.js
+++ js/home.js
@@ -131,7 +131,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     // ======================
     async function loadProducts() {
         try {
-            const res = await fetch('/.netlify/functions/get-products', {
+            const res = await fetch('api/get-products', {
                 method: 'GET',
                 credentials: 'include'
             });
